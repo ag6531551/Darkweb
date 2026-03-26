6432e4e443749399c5fdcc93f614ee2df2196489
ag6531551 <ag6531551@gmail.com>
2026-03-26 13:43:24 +0530

    Create Darkweb.lua with a clean Lua script

---
+++ Darkweb.lua
@@ -0,0 +1,10 @@
+-- Darkweb.lua
+-- This is a complete clean Lua script example
+
+-- Function to display a simple message
+function displayMessage()
+    print("Welcome to the Darkweb script!")
+end
+
+-- Call the function
+displayMessage()
